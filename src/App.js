c41089d5d393fca00cdf022b29cfc20938c50560
wormjim <gcooled29@gmail.com>
2019-08-29 23:41:55 -0400

    Update App Component For Base Components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import Posting from './Containers/Posting/Posting';
+import Header from './Components/Header/Header';
+import Auth from './Components/Auth/Auth';
+
 // import Filter from './Components/Filter/Filter';
 
+import { fetchFromDB } from './util/fetch';
+import AuthContext from './HoC/auth-context';
 // import './App.css';
 
-import { fetchFromDB } from './util/fetch';
-
 export default ({ user }) => {
+  const [page, setPage] = useState('auth');
   const [info, setInfo] = useState([]);
+  const [authState, setAuthState] = useState(false);
 
   useEffect(() => {
     fetchFromDB(`posting/companies?user=marc`, setInfo);
   }, []);
 
-  useEffect(() => {
-    console.log('Info =>', info);
-  });
+  const switchPage = (pageName) => {
+    setPage(pageName);
+  };
+
+  const login = () => {
+    console.log('In Auth');
+
+    setAuthState(true);
+  };
 
   return (
     <>
-      <Posting
-        user={'marc'}
-        id={'5c90654a6b802d292c9308ae'}
-        data={info}
-        filled={true}
-      />
-      {/* <Filter user={'marc'} /> */}
+      <AuthContext.Provider value={{ state: authState, login: login }}>
+        <Header
+          onLoadPostings={switchPage.bind(this, 'postings')}
+          onLoadAuth={switchPage.bind(this, 'auth')}
+        />
+        <hr />
+        {page === 'auth' ? (
+          <Auth />
+        ) : (
+          <Posting
+            user={'marc'}
+            id={'5c90654a6b802d292c9308ae'}
+            data={info}
+            filled={true}
+          />
+        )}
+        {/* <Filter user={'marc'} /> */}
+      </AuthContext.Provider>
     </>
   );
 };
