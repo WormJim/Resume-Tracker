2e47eda4abe8bab0852d90bb729194857e230c7f
wormjim <gcooled29@gmail.com>
2019-09-02 17:20:37 -0400

    (WIP) Comment Out Config File

---
+++ server/config/default.ts
@@ -0,0 +1,12 @@
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// export default {
+//   PORT: 5050,
+//   DATABASE: {
+//     HOST: process.env.HOST,
+//     USER: process.env.USER,
+//     PASS: process.env.PASS,
+//     BASE: process.env.BASE,
+//   },
+// };
