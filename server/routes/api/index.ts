9419bc46748eccff601bf3d33f2102e5c229cec1
wormjim <gcooled29@gmail.com>
2019-09-02 23:22:23 -0400

    Move Posting Route To Posting File

--- server/routes/api/index.ts
+++ server/routes/api/index.ts
@@ -1,89 +1,16 @@
-// import express from 'express';
-// import { ObjectID } from 'mongodb';
+import express from 'express';
 
-// // const pipline = require('./agg');
+// const pipline = require('./agg');
 
-// export default async function apiRouter(dbClient: any) {
-//   const router = express.Router();
+export default async function apiRouter(dbClient: any) {
+  const router = express.Router();
 
-//   router.post('/posting/create', (req, res) => {
-//     const { user } = req.query;
+  const short = await new Promise((resolve, reject) => (true ? resolve('s') : reject(new Error())));
 
-//     const test = dbClient.collection(user);
+  console.info(short);
 
-//     test
-//       .insertOne(req.body)
-//       .then((result) => {
-//         res.send(result);
-//       })
-//       .catch((err) => {
-//         res.status(500).send(err);
-//       });
-//   });
+  router.use('/users');
+  router.use('/tracker');
 
-//   router.get('/posting/companies', (req, res) => {
-//     const { user } = req.query;
-
-//     const collection = dbClient.collection(user);
-
-//     collection
-//       .aggregate(pipline)
-//       .toArray()
-//       .then((data) => {
-//         res.send(data);
-//       })
-//       .catch((err) => {
-//         res.status(500).send(err);
-//       });
-//   });
-
-//   router.get('/posting/company', (req, res) => {
-//     const { user } = req.query;
-//     const { _id } = req.body;
-
-//     const collection = dbClient.collection(user);
-
-//     collection
-//       .findOne(ObjectID(_id))
-//       .then((data) => {
-//         res.send(data);
-//       })
-//       .catch((err) => {
-//         res.status(500).send(err);
-//       });
-//   });
-
-//   router.put('/posting/update', (req, res) => {
-//     const { user } = req.query;
-//     const { _id } = req.body;
-
-//     const collection = dbClient.collection(user);
-
-//     collection
-//       .findOneAndUpdate(ObjectID(_id), req.body)
-//       .then((data) => {
-//         res.send(data);
-//       })
-//       .catch((err) => {
-//         res.status(500).send(err);
-//       });
-//   });
-
-//   router.put('/posting/update/stage', (req, res) => {
-//     const { user } = req.query;
-//     const { _id } = req.body;
-
-//     const collection = dbClient.collection(user);
-
-//     collection
-//       .updateOne(ObjectID(_id), { $push: {} })
-//       .then((data) => {
-//         res.send(data);
-//       })
-//       .catch((err) => {
-//         res.status(500).send(err);
-//       });
-//   });
-
-//   return router;
-// }
+  return router;
+}
