60b42d69f6d3acc8d8be4401d32f48dee04b14d5
wormjim <gcooled29@gmail.com>
2019-09-24 13:47:54 -0400

    Export Link Component From Index

--- src/Atoms/index.ts
+++ src/Atoms/index.ts
@@ -3,3 +3,6 @@ export * from 'src/Atoms/Card/Card';
 
 export { default as TextInput } from 'src/Atoms/TextInput/TextInput';
 export * from 'src/Atoms/TextInput/TextInput';
+
+export { default as Link } from 'src/Atoms/Link/Link';
+export * from 'src/Atoms/Link/Link';
