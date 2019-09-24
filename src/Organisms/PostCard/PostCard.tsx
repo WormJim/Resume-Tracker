3185d60c7e064657c70344f20c5e6a0eafc759ec
wormjim <gcooled29@gmail.com>
2019-09-24 12:53:53 -0400

    Structure Post Card Component Outline

---
+++ src/Organisms/PostCard/PostCard.tsx
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/styles';
+import React, { memo } from 'react';
+import { Card } from 'src/Atoms';
+
+const useStyles = makeStyles({});
+
+const PostCard = () => <Card> </Card>;
+
+export default memo(PostCard);
