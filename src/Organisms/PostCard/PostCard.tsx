df7b894673ea18885ecc4655a857e51c6f82c31b
wormjim <gcooled29@gmail.com>
2019-09-24 12:56:50 -0400

    Import Mock Data

--- src/Organisms/PostCard/PostCard.tsx
+++ src/Organisms/PostCard/PostCard.tsx
@@ -1,6 +1,7 @@
 import { makeStyles } from '@material-ui/styles';
 import React, { memo } from 'react';
 import { Card } from 'src/Atoms';
+import mockData from 'src/mockData';
 
 const useStyles = makeStyles({});
 
