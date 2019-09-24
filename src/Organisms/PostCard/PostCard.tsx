4a17d2fde0cc2f1e81721b09ce0e329ee12dcac7
wormjim <gcooled29@gmail.com>
2019-09-24 13:09:21 -0400

    Set Base Structure Of Post Card

--- src/Organisms/PostCard/PostCard.tsx
+++ src/Organisms/PostCard/PostCard.tsx
@@ -1,10 +1,21 @@
 import { makeStyles } from '@material-ui/styles';
 import React, { memo } from 'react';
 import { Card } from 'src/Atoms';
-import mockData from 'src/mockData';
 
 const useStyles = makeStyles({});
 
-const PostCard = () => <Card> </Card>;
+interface PostCardProps {
+  source: { position: string };
+}
+
+const PostCard = ({ source }: PostCardProps) => {
+  const styles = useStyles();
+
+  return (
+    <Card variant="medium">
+      <h2>{source.position}</h2>
+    </Card>
+  );
+};
 
 export default memo(PostCard);
