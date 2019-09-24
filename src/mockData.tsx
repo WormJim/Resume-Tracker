e3c1783ddff43d1486d196c06f564dad4a4ca215
wormjim <gcooled29@gmail.com>
2019-09-24 12:54:38 -0400

    Create Mock Data

---
+++ src/mockData.tsx
@@ -0,0 +1,132 @@
+const mockData = [
+  {
+    company: 'GrubHub',
+    position: 'Front End Engineer 1',
+    dateAdded: '09/20/2019',
+    applied: true,
+    dateApplied: '09/20/2019',
+    motivation: 4,
+    connection: false,
+    connectionName: null,
+    messaged: null,
+    initial: 'No Response',
+    onSite: null,
+    offer: null,
+    reference:
+      'https://careers-grubhub.icims.com/jobs/8906/front-end-engineer-i/job?mode=submit_apply',
+    details: `About The Opportunity
+      Tens of thousands of hungry Diners utilize our platform daily. Our goal is to find as many hungry people who aren’t currently using the platform and make sure that they get fed. As part of the growth team, you’ll be focussing on rolling out a new SEO mobile user focussed landing experience, improving onboarding and diner retention through targeted multichannel communications.
+      
+      Some Challenges You’ll Tackle
+      Design, develop and deploy awesome technologies and products for a range of users (diners, corporate accounts, restaurants, internal tools).
+      Build software based on an understanding of business and technical priorities formed through tight collaboration with leadership, product owners, other technologists, and users.
+      Solve complex problems for diners, restaurateurs and internal stakeholders using elegant technical solutions.
+      Contribute to our ever evolving software architecture and development methodologies.
+      You Should Have
+      A Bachelor’s Degree in Computer Science, Software Engineering or a related field.
+      A track record of strong academic performance (3.0 or higher cumulative GPA).
+      A solid foundation in software programming with a modern, general purpose language such as:  Java, Javascript/Typescript/Node, Python, C#.
+      A passion for building applications used by millions of people in a team-oriented environment.
+      Creativity and analytical abilities required for solving complex problems.
+      Familiarity with Agile software development methodologies.
+      A desire to join a fast paced tech- and product- oriented company.
+      And Of Course, Perks!
+      And Of Course, Perks!
+      
+      Flexible PTO. It’s true, no strings attached and all the time you need to recharge. 
+      PTO. It’s true, we provide you a generous amount of time to recharge. 
+      Better Benefits. Get quality insurance, flex-spending accounts, retirement options and commuter perks.  
+      Free Food. Kitchens are stocked and free Grubhub each week. 
+      Stock Up. All of our employees are owners, in fact, they’re granted Restricted Stock Units, which means we’re all in it to win it. 
+      Casual Culture. Catch rays on the rooftop or get comfy on a couch and get to know your coworkers — because work, should be a place you want to be. 
+      
+      
+      
+      Grubhub is an equal opportunity employer. We evaluate qualified applicants without regard to race, color, religion, sex, sexual orientation, gender identity, national origin, disability, veteran status, and other legally protected characteristics. The EEO is the Law poster is available here: DOL Poster. If you are applying for a job in the U.S. and need a reasonable accommodation for any part of the employment process, please send an e-mail to talentacquisition@grubhub.com and let us know the nature of your request and contact information. Please note that only those inquiries concerning a request for reasonable accommodation will be responded to from this e-mail address.`,
+  },
+  {
+    company: 'CBOE',
+    position: 'Software Engineer',
+    dateAdded: '09/20/2019',
+    applied: true,
+    dateApplied: '09/20/2019',
+    motivation: 4,
+    connection: false,
+    connectionName: null,
+    messaged: null,
+    initial: 'In Process',
+    onSite: null,
+    offer: null,
+    reference:
+      'https://cboe.wd1.myworkdayjobs.com/en-US/External_Career_CBOE/job/Software-Engineer_R-0716-1',
+    details: `Job Description
+    This position entails building/maintaining software applications and web interfaces which are highly reliable (24hrs) that support:
+    
+    Collecting/aggregating data on market activity
+    Operational tools
+    Flexible applications to query, summarize, and analyze market activity
+    Automated identification and alerting of anomalous market events
+    Cboe FX external website
+    Historic market data product
+    The right candidate is responsible for:
+    
+    Capturing/refining requirements with relevant stakeholders
+    Designing data storage schema
+    Collecting/storing relevant data
+    Auto archiving older records
+    Providing a means of access to the data in the various ways it will be needed
+    Writing unit tests
+    Documenting relevant details in the team wiki
+    Job Requirements:
+    
+    Two to five years’ experience.
+    Strong communication skills, and ability to work with staff in operations and sales.
+    Bachelor’s Degree in Computer Science or related field preferred.
+    Strong knowledge of object oriented concepts, data structures, and algorithms.
+    Experience with Python and one of Java or C++.
+    Web development experience in an MVC environment, preferably with Django and AngularJS.
+    Experience with PostgreSQL or other RDBMS, and Linux/Unix operating systems.
+    Experience using Git/version control, MongoDB, Redis, Jenkins, and working with very large data sets is desirable.
+    Small company experience a plus.
+    Self-directed, self-motivated.`,
+  },
+  {
+    company: 'The Wing',
+    position: 'JavaScript Engineer (Backend)',
+    dateAdded: '09/20/2019',
+    applied: true,
+    dateApplied: '09/20/2019',
+    motivation: 4,
+    connection: false,
+    connectionName: null,
+    messaged: null,
+    initial: 'Rejected',
+    onSite: null,
+    offer: null,
+    reference: 'https://jobs.lever.co/the-wing/c3e00e63-1eee-4f48-a4fd-8f4679288967',
+    details: `The Wing is scaling its product and engineering teams as part of our efforts to  provide our rapidly growing community of members with the ability to connect with each other anytime, anywhere.
+
+    The Wing is seeking a mission-driven JavaScript engineer to join our rapidly scaling team. In this role, you will work in a small, autonomous product team to discover, develop, and ship valuable, seamless, and delightful products for our users. Reporting directly to the Director of Engineering, you will play a critical role in establishing an engineering process that promotes shipping frequently and predictably, while uncovering risk. If you love a fast-paced environment and working for mission-driven organization, this opportunity could be for you.
+    Responsibilities include but are not limited to:
+    Collaborate with other engineers, product managers, and product designers to shape and deliver on the product roadmap
+    Assess business and product requirements and propose viable technical solutions
+    Write clean, maintainable code and build reusable components that can scale
+    Write both unit and integration tests
+    Participate in sprint and stand-up process
+    Take active role in estimation process
+    Participate in pair programming with other engineers and in peer code review process
+    Contribute to recruiting efforts, including interviewing and optimizing our hiring process
+    Qualifications:
+    3-5 years of relevant engineering work experience
+    Proven track record of building and shipping high-quality software
+    Expert knowledge of Express, Node.js
+    Experience integrating with various APIs and external services
+    Experience working in agile product teams
+    Excellent communicator; able to convey intricate systems and logic to both technical and non-technical partners
+    Key traits we're looking for:
+    Comfortable working in a high-growth, fast-paced and iterative environment
+    Energized by solving complex, ambiguous problems
+    Desire to roll up sleeves and get scrappy
+    Curious, collaborative, and empathetic`,
+  },
+];
