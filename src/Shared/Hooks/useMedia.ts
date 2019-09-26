345c28c2b8f2de7a42853f7f0be1edaa48fe93fb
wormjim <gcooled29@gmail.com>
2019-09-26 00:38:52 -0400

    Set Large Media Query

--- src/Shared/Hooks/useMedia.ts
+++ src/Shared/Hooks/useMedia.ts
@@ -2,3 +2,4 @@ import { useMediaQuery } from '@material-ui/core';
 
 export const useSmallMedia = useMediaQuery('(min-width:576px)');
 export const useMediumMedia = useMediaQuery('(min-width:768px)');
+export const useLargeMedia = useMediaQuery('(min-width:992px)');
