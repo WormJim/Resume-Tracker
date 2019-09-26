362ce55c1e617c69cbf18eba54fe08d4a59eff25
wormjim <gcooled29@gmail.com>
2019-09-26 10:27:11 -0400

    Export Media Qs From Index

--- src/Molecules/index.ts
+++ src/Molecules/index.ts
@@ -1,2 +1,5 @@
 export { default as PostCard } from 'src/Molecules/PostCard/PostCard';
 export * from 'src/Molecules/PostCard/PostCard';
+
+export { default as SiteLayout } from 'src/Molecules/Layout/Layout';
+export * from 'src/Molecules/Layout/Layout';
