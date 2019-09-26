e6678338dbf3f19b08e65af7c3979c094a1c7d94
wormjim <gcooled29@gmail.com>
2019-09-26 00:38:32 -0400

    Set Medium Media Query

--- src/Shared/Hooks/useMedia.ts
+++ src/Shared/Hooks/useMedia.ts
@@ -1,3 +1,4 @@
 import { useMediaQuery } from '@material-ui/core';
 
 export const useSmallMedia = useMediaQuery('(min-width:576px)');
+export const useMediumMedia = useMediaQuery('(min-width:768px)');
