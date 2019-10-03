f0341d1665b4d2b20be4fd31762eb5f86cda7a13
wormjim <gcooled29@gmail.com>
2019-10-03 17:48:50 -0400

    Export Test Route

--- src/Organisms/index.ts
+++ src/Organisms/index.ts
@@ -1,2 +1,5 @@
 export { default as Postings } from 'src/Organisms/Postings/Postings';
 export * from 'src/Organisms/Postings/Postings';
+
+export { default as Routes } from 'src/Organisms/Routes/Routes';
+export * from 'src/Organisms/Routes/Routes';
