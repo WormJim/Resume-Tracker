be18d47e7aa6f668eae148465e746c1ec429d7ed
wormjim <gcooled29@gmail.com>
2019-10-07 13:52:38 -0400

    Position Content Between

--- src/Molecules/Sidebar/Sidebar.tsx
+++ src/Molecules/Sidebar/Sidebar.tsx
@@ -4,15 +4,25 @@ import React, { memo, ReactNode } from 'react';
 import { Card } from 'src/Atoms';
 
 const useStyles = makeStyles({
-  root: {},
+  root: {
+    width: '250px',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
 });
 
 interface SidebarProps {
   children?: ReactNode;
   className?: string;
+  content?: string[];
+  footer?: ReactNode;
+  header?: ReactNode;
 }
 
-const Sidebar = ({ children, className }: SidebarProps) => {
+const Sidebar = ({ children, className, content, footer, header }: SidebarProps) => {
   const styles = useStyles();
 
   return (
