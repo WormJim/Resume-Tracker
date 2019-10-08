cc90ef27d9b721dba7c03da93dbf7ab25df8ec38
wormjim <gcooled29@gmail.com>
2019-10-08 00:15:59 -0400

    Export Dashbord and App from Index

--- src/Organisms/index.ts
+++ src/Organisms/index.ts
@@ -1,3 +1,9 @@
+export { default as App } from 'src/Organisms/App/App';
+export * from 'src/Organisms/App/App';
+
+export { default as Dashboard } from 'src/Organisms/Dashboard/Dashboard';
+export * from 'src/Organisms/Dashboard/Dashboard';
+
 export { default as Postings } from 'src/Organisms/Postings/Postings';
 export * from 'src/Organisms/Postings/Postings';
 
