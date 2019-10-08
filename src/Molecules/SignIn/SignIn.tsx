fe8abd6f8db4edf07c0a10b0ffd2f616528546b2
wormjim <gcooled29@gmail.com>
2019-10-08 18:51:19 -0400

    Set routeing In Login Click For Development

--- src/Molecules/SignIn/SignIn.tsx
+++ src/Molecules/SignIn/SignIn.tsx
@@ -16,6 +16,15 @@ const SignIn = ({ children, className }: SignInProps) => {
   const styles = useStyles();
   const history = useHistory();
 
+  const cookies: string = document.cookie;
+
+  const loginClick = useCallback(() => {
+    const age = 60 * 5;
+
+    document.cookie = `login=true; max-age=${age}`;
+    history.push('/portal/dashboard');
+  }, [history]);
+
   return (
     <Card className={classNames(styles.root, className)}>
       <CardContent>
