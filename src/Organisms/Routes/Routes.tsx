3fdbd36a12b4b31725f038447321d5d1b5050224
wormjim <gcooled29@gmail.com>
2019-10-08 16:53:40 -0400

    Add Lazy Loading Capabilities to Routes

--- src/Organisms/Routes/Routes.tsx
+++ src/Organisms/Routes/Routes.tsx
@@ -1,11 +1,22 @@
-import React, { Suspense, useCallback, ReactNode } from 'react';
+import React, { Suspense, useCallback, ReactNode, lazy } from 'react';
 import { HashRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import { App, Dashboard, Postings } from 'src/Organisms';
 
 const useAppRender = (child: ReactNode) => useCallback(() => <Route>{(rotuerProps) => <App>{child}</App>}</Route>, [child]);
 
+const useLazyAppRender = <T extends React.ComponentType<any>>(
+  cb: () => Promise<{ default: T }>,
+) => {
+  const LazyComponent = lazy(cb) as any;
+
+  return useAppRender(<LazyComponent />);
+};
+
 const Routes = () => {
   const renderDashBoard = useAppRender(<Dashboard />);
+
+  const renderDashBoardLazy = useLazyAppRender(() => import('src/Organisms/Dashboard/Dashboard'));
+  const renderListLazy = useLazyAppRender(() => import('src/Organisms/Postings/Postings'));
 
   return (
     // Wrap In Layout
@@ -21,13 +32,15 @@ const Routes = () => {
         </ul>
       </nav>
 
-      <Switch>
-        <Route exact path="/list">
-          <Postings />
-          {/* <SigninScreen></SigninScreen> */}
-        </Route>
-        <Route exact path="/portal/dashboard" render={renderDashBoard} />
-      </Switch>
+      <Suspense fallback={<Postings />}>
+        <Switch>
+          <Route exact path="/list" render={renderListLazy}>
+            {/* <Postings /> */}
+            {/* <SigninScreen></SigninScreen> */}
+          </Route>
+          <Route exact path="/portal/dashboard" render={renderDashBoardLazy} />
+        </Switch>
+      </Suspense>
     </Router>
   );
 };
